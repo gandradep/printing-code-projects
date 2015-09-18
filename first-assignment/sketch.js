8511439db06eac8fee81927f7cfc8b81d2b93b86
Kristina Budelis <kbudelis@gmail.com>
2015-09-18 17:30:18 -0400

    homework

--- first-assignment/sketch.js
+++ first-assignment/sketch.js
@@ -1,20 +1,56 @@
+
+// var r = new Rune({
+//   container: "body",
+//   width: 600,
+//   height: 842,
+//   debug: true
+// });
+
+// r.rect(0,r.height/2, r.width, r.height/2)
+//   .fill(0)
+
+//   .stroke(false)
+
+//   var diameter = r.width*0.1
+  
+
+//   r.circle(r.width*2/3,r.height/2, diameter)
+//   .fill(0)
+//   .stroke(false)
+
+// r.triangle((r.width*2/3)-diameter, r.height/2, (r.width*2/3)+diameter, r.height/2, r.width*2/3,(r.height/2)+(diameter*2))
+//   .fill(255)
+
+//   .stroke(false)
+
+
+
+// r.draw();
+
+// different colors
+
 var r = new Rune({
-  container: "#canvas",
-  width: 800,
-  height: 800,
+  container: "body",
+  width: 600,
+  height: 845,
   debug: true
 });
 
-r.rect(100, 100, 200, 200)
-  .fill(255, 0, 0)
-  .stroke(0)
-
-r.triangle(400, 50, 400, 400, 650, 50)
-  .fill(0, 255, 0)
-  .stroke(0)
-
-r.ellipse(200, 500, 200, 300)
-  .fill(0, 0, 255)
+r.rect(0,0, r.width, r.height/2)
+  .fill(0)
   .stroke(false)
+
+  var diameter = r.width*0.1
+  
+
+  r.circle(r.width/3,r.height/2, diameter)
+  .fill(255)
+  .stroke(false)
+
+r.triangle((r.width/3)-diameter, r.height/2, (r.width/3)+diameter, r.height/2, r.width/3,(r.height/2)+(diameter*2))
+
+  .fill(0)
+  .stroke(false)
+
 
 r.draw();
